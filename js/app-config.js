2f7fc00b5038f1392b1206967ff6ca959c95017d
muya <kingkonig@gmail.com>
2014-05-11 17:13:32 +0300

    updated manifest, description
    -updated manifest to remove reference to localhost
    -updated manifest to have new description
    -changed default action to preview

--- js/app-config.js
+++ js/app-config.js
@@ -1,5 +1,5 @@
 var Config = {
 	"api_response_format": "json",
-	"default_expand_url_action": "redirect",
-	"expand_url_api": ""
+	"default_expand_url_action": "preview",
+	"expand_url_api": "http://muya.co.ke/expand-url/"
 };
